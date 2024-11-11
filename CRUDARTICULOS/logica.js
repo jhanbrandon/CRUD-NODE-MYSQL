a4138dfcb86e4e3dffa6bbc40e8bb8127a4c0592
Jhan <jhanbrandon1@gmail.com>
2024-11-11 16:32:02 -0500

    version original profesor 001

---
+++ CRUDARTICULOS/logica.js
@@ -0,0 +1,86 @@
+const url="http://localhost:3000/api/articulos"
+let id=document.getElementById("cjaid")
+const urlbusquedaunitaria='http://localhost:3000/api/articulos/'+id
+ 
+const contenedor=document.querySelector('tbody')
+let resultados=''
+
+
+
+const modalarticulo=new bootstrap.Modal(document.getElementById('modalarticulo'))
+const formarticulo=document.querySelector('form')
+const descripcion=document.getElementById("descripcion")
+const precio=document.getElementById("precio")
+const stock=document.getElementById("stock")
+let opcion=''
+
+btncrear.addEventListener("click",()=>{
+    descripcion.value=""
+    precio.value=""
+    stock.value=""
+    modalarticulo.show()
+    opcion="crear"
+
+})
+
+//funcion para mostrar articulos
+const mostrar=(articulos)=>{
+    articulos.forEach(articulo => {
+        resultados+= `<tr>
+
+            <td>${articulo.id}</td>
+            <td>${articulo.descripcion}</td>
+            <td>${articulo.precio}</td>
+            <td>${articulo.stock}</td>
+            <td><a              
+                id="editar"
+                class="btn btn-primary"                
+                role="button"
+                >Editar</a
+            ><a              
+                id="eliminar"
+                class="btn btn-danger"                
+                role="button"
+                >Eliminar</a
+            >
+            </td>
+           
+        </tr>`
+    });
+    contenedor.innerHTML=resultados
+}
+
+//procedimiento para mosrtar
+fetch(url)
+  .then(response=>response.json())
+  .then(data=>mostrar(data))
+  .catch(error=>console.log(error))
+
+
+
+
+//metodo para crear
+formarticulo.addEventListener('submit',(e)=>{
+
+    e.preventDefault()
+    if(opcion=="crear"){
+        fetch(url,{
+            method:'POST',
+            headers:{'Content-Type': applications/json},
+        body: JSON.stringify({
+            descripcion:descripcion.value,
+            precio:precio.value,
+            stock:stock.value
+
+        })
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        const nuevoArticulo=[]
+        nuevoArticulo.push(data)
+        mostrar(nuevoArticulo)
+    })
+}
+
+modalarticulo.hidde
+    })
